071ba65f0cc9aeff037b9197fa7e7b506a78f79c
Mathew Woodyard <mat@matwoodyard.com>
2019-02-15 19:25:37 -0600

    Remove epel from test containers and add tests to make sure EPEL repositories are usable.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,19 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_epel_repo_file_should_exist(host):
+    epel_repo_file = host.file('/etc/yum.repos.d/epel.repo')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert epel_repo_file.exists
+
+
+def test_yum_should_install_packages_in_epel_repo(host):
+    yum_install_return_code = host.run('yum install -y htop').rc
+
+    assert yum_install_return_code == 0
+
+
+def test_installed_package_should_run(host):
+    htop_return_code = host.run('htop --version').rc
+
+    assert htop_return_code == 0
